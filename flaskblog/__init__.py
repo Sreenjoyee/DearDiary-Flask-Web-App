c91bfa60caadaa13e7bd53f29e21537975ed30be
Sreenjoyee Saha <sreenjoyeesaha@gmail.com>
2025-06-27 10:05:52 +0530

    switched to postgres as a db cause sqlite not suitable for deploy

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,7 +11,7 @@ dotenv.load_dotenv()
 app = Flask(__name__)  
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
